9e70b8c975625b69eb392755e9b73955c8bf5cc2
Beau Kinstler <git@beaukinstler.info>
2019-02-07 13:13:41 -0500

    refactor: update tests base on my new method and adapt to new func

--- general_functions/testing_finding_email_fn.sql
+++ general_functions/testing_finding_email_fn.sql
@@ -1,32 +1,73 @@
+
+
 USE [Sandbox]
 GO
 
-declare @x as varchar(max)
--- should return fdslk@sdlfkjascom
-SELECT @x= [dbo].[fn_get_email_from_string] (
-   '    first fdslk@sdlfkjascom last ', ' ')
-if @x = 'fdslk@sdlfkjascom'
+/*
+ *  Tests for dbo.[fn_get_email_from_string]
+ *  Tests pass if they return 1
+ *  If they return a table, it shows that they havn'te returned the proper value, but shows 
+ *  the result, and what was expected.
+*/
+
+declare @result as varchar(max)
+declare @param as varchar(max)
+declare @exp_result as varchar(max)
+declare @delim as varchar(1)
+
+
+set @delim = ' '
+set @param = @delim + @delim + 'tes-t@exam-ple4.com' + @delim  + @delim
+set @exp_result = 'tes-t@exam-ple4.com'
+SELECT @result= [dbo].fn_get_email_from_string (@param, @delim)
+if @result = @exp_result
     select 1;
 else
-    select 0;
-GO
+    select @result as  'result', @exp_result as 'expected';
 
 
--- should return 'first@.com, fdslk@sdlfkjas.com' last
-SELECT @x= [dbo].[fn_get_email_from_string] (
-   '    first@.com fdslk@sdlfkjas.com last ', ' ')
-if @x = 'first@.com, fdslk@sdlfkjas.com'
+set @delim = ':'
+set @param = @delim + @delim + 'tes-t@exam-ple4.com' + @delim  + @delim
+set @exp_result = 'tes-t@exam-ple4.com'
+SELECT @result= [dbo].fn_get_email_from_string (@param, @delim)
+if @result = @exp_result
     select 1;
 else
-    select 0;
-GO
+    select @result as  'result', @exp_result as 'expected';
 
--- should return 'fdslk@sdlfkjas.com, last@.com'
-SELECT @x= [dbo].[fn_get_email_from_string] (
-   '    first,fdslk@sdlfkjas.com,last@.com ', ',')
-if @x = 'fdslk@sdlfkjas.com, last@.com'
+set @delim = ' '
+set @param = 'first' + @delim + 'fdslk@sdlfkjascom' + @delim  + 'last '
+set @exp_result = NULL
+SELECT @result= [dbo].fn_get_email_from_string (@param, @delim)
+if @result IS NULL
     select 1;
 else
-    select 0;
-GO
+    select @result as  'result', @exp_result as 'expected';
 
+set @delim = ','
+set @param = 'first' + @delim + 'fdslk@sdlfkjascom' + @delim  + 'last '
+set @exp_result = NULL
+SELECT @result= [dbo].fn_get_email_from_string (@param, @delim)
+if @result IS NULL
+    select 1;
+else
+    select @result as  'result', @exp_result as 'expected';
+
+set @delim = ' '
+set @param = 'first@.com' + @delim + 'fdslk@sdlfkjas.com' + @delim  + 'last '
+set @exp_result = 'fdslk@sdlfkjas.com'
+SELECT @result= [dbo].fn_get_email_from_string (@param, @delim)
+if @result = @exp_result
+    select 1;
+else
+    select @result as  'result', @exp_result as 'expected';
+
+
+set @delim = ','
+set @param = 'first@.com' + @delim + 'fdslk@sdlfkjas.com' + @delim  + 'last '
+set @exp_result = 'fdslk@sdlfkjas.com'
+SELECT @result= [dbo].fn_get_email_from_string (@param, @delim)
+if @result = @exp_result
+    select 1;
+else
+    select @result as  'result', @exp_result as 'expected';
